ff9464b8e4ee313c5dfe7585639294af50c3d6b4
daniel kirschner <dpkirschner@gmail.com>
2015-04-08 21:36:40 -0700

    adding show html and index -> show links

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,7 +1,7 @@
 module ApplicationHelper
   # Returns the full title on a per-page basis.
   def full_title(page_title = '')
-    base_title = "Prompt Me"
+    base_title = "LoL Ref"
     if page_title.empty?
       base_title
     else
