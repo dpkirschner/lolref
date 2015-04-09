7c70ad65141b110f3071e35d1669ec6068e27d09
daniel kirschner <dpkirschner@gmail.com>
2015-04-08 20:59:50 -0700

    base skeleton pages + css

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,16 @@
 Rails.application.routes.draw do
   
   resources :champion
+  root 'static_pages#home'
   
-  root 'champion#index'
+  get 'help'    => 'static_pages#help'
+  get 'about'   => 'static_pages#about'
+  get 'contact' => 'static_pages#contact'
+ 
+  get 'champion/:id'    => 'champion#show'
+  get 'champion'        => 'champion#index'
+  
+  
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
